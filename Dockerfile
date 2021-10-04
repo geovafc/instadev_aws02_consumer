5d24eefbd8b99bcf82c43089dd355ddfe99714f0
Geovane Freitas <geovane.freitasc@gmail.com>
2021-10-04 11:30:44 -0300

    Creating image docker and push docker hub

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+#Esse arquivo descreve como vamos montar a nossa imagem.
+#Ele fica na raiz do projeto
+#Ele contém instruções para gerar imagem e para dizer a partir de qual imagem vamos gerar a nossa imagem
+
+#Essa imagem será gerada a partir da imagem do openjdk:15 que possui tudo que agente precisa para
+#executar a nossa aplicação, inclusive o Java 16
+#FROM openjdk:16
+#VOLUME /tmp
+#ARG DEPENDENCY=build/dependency
+#COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
+#COPY ${DEPENDENCY}/META-INF /app/META-INF
+#COPY ${DEPENDENCY}/BOOT-INF/classes /app
+##Ponto de entrada significa: quando o docker for executar a nossa aplicação, qual será a classe
+## que ele vai chamar para ser executada ? tenho que colocar o endereço da minha classe principal
+#ENTRYPOINT ["java","-cp","app:app/lib/*","br.com.instadev.aws_instadev02.AwsInstadev02Application"]
+
+FROM adoptopenjdk/openjdk11:jre-11.0.4_11
+VOLUME /tmp
+ARG JAR_FILE
+COPY ${JAR_FILE} app.jar
+# COPY --from=builder /usr/src/app/build/libs/demo-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,106 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+var springBootVersion = ""
+var url = ""
+
 
 plugins {
-	id("org.springframework.boot") version "2.5.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.31"
-	kotlin("plugin.spring") version "1.5.31"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    //    Aplica o plugin que irá gerar a imagem do docker
+    id("com.palantir.docker") version "0.22.1"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
-group = "br.com.instadev"
+//Esse processo de build será feito quando formos gerar a imagem do docker
+buildscript {
+//    ext {
+//        springBootVersion = "2.5.5-SNAPSHOT"
+//    }
+
+//repositórios que vamos utilizar
+    repositories {
+        mavenCentral()
+        maven {
+            url =  uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    //Bibliotecas que serão usadas durante o processo de build
+    dependencies {
+//        Biblioteca do spring boot
+//        classpath("org.springframework.boot:spring-boot-gradle-plugin:${springBootVersion}")
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.5.5-SNAPSHOT")
+//        Biblioteca que irá criar tarefas no intellij para poder executar a criação da imagem do docker
+        classpath("gradle.plugin.com.palantir.gradle.docker:gradle-docker:0.22.1")
+    }
+}
+
+//Adiciona o id do docker hub
+group = "fcgeovane"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+
+////Início da configuração que irá gerar a imagem do docker baseado com as informações do projeto
+
+
+// Gradle Docker plugin configuration
+// Please make sure you login to Docker registry before running Docker related tasks
+docker {
+    // All the build process should be passed before we run Docker related tasks
+    dependsOn(tasks.getByName("build"))
+
+    val bootJar: BootJar by tasks
+
+    // Please specify the image metadata here
+    val userName = "fcgeovane"
+    val version = "1.0.0"
+    name = "$userName/${project.name}:$version"
+
+    // Please add the tags if you need more registries/userNames/tags.
+    // Accordingly this plugin will create a corresponding task to tag/push it.
+    //
+    // By default, the registry to which it will push when you run "dockerPush" task is "docker.io".
+    // So practically the following are not needed.
+    //
+    // val registry = "docker.io"
+    // tag("DockerIO", "$registry/$name")
+
+    // Set the path to Dockerfile
+    setDockerfile(file("Dockerfile"))
+
+    // Add the built jar file to Docker's build context
+    files(bootJar.archiveFile)
+
+    // Set buildArgs of Dockerfile
+    buildArgs(mapOf(
+        "JAR_FILE" to bootJar.archiveFileName.get()
+    ))
+}
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation ("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
