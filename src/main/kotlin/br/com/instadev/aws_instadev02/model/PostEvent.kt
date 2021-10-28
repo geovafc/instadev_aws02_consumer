4680f0ea3b102b9801d29c91d951e92005a3976f
Geovane Freitas <geovane.freitasc@gmail.com>
2021-10-28 12:22:57 -0300

    Ajustes postEvent

--- src/main/kotlin/br/com/instadev/aws_instadev02/model/PostEvent.kt
+++ src/main/kotlin/br/com/instadev/aws_instadev02/model/PostEvent.kt
@@ -1,5 +1,5 @@
 package br.com.instadev.aws_instadev02.model
 
-data class PostEvent( var postId: Long, var code:String, var username:String   ) {
+data class PostEvent( var postId: Long, var userName:String   ) {
 
 }
